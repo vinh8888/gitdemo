b8575f9a4cdba25c7e6967ffded056fcb1e8afdc
Vinh Vo <voquangvinh@gmail.com>
2023-05-14 00:07:36 +0700

    combination

---
+++ demogit/src/main/java/com/vinh/demo/DemogitApplication.java
@@ -0,0 +1,20 @@
+package com.vinh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitApplication {
+
+
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogitApplication.class, args);
+	}
+
+	
+	
+	
+}
