aae4b320b989083434cb4414873a10890b4f405b
Vinh Vo <voquangvinh@gmail.com>
2023-05-14 00:07:36 +0700

    This is a combination of 2 commits.
    combination
    
    m2

---
+++ demogit/src/test/java/com/vinh/demo/DemogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
